248366b4e046cdd511294a95250d4959e280eae9
kaiohenrikk <kaiohenrik.profissional@gmail.com>
2022-10-25 09:12:23 -0300

    ajustando home

--- src/MinhaCompra/src/views/Home.js
+++ src/MinhaCompra/src/views/Home.js
@@ -1,17 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect, useReducer } from 'react';
+import { StyleSheet, Text, View, Entypo } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import SearchBar from '../components/SearchBar';
 import Lists from '../components/Lists';
-
-import Profile from './Profile'
-import Statistic from './Statistic';
-import Lista from './Lista';
+import ButtonFab from '../components/ButtonFab';
 
 const Tap = createBottomTabNavigator()
 
 export default function Home() {
+  const [user, setUser] = ([])
+
   return (
     <View style={styles.container}>
 
@@ -19,7 +17,17 @@ export default function Home() {
 
       <SearchBar />
 
-      <Lists name='Lista 1' />
+      {!user ?
+
+        <View style={styles.noListsView}>
+          <Text style={{fontSize: 13}}>Clique no ícone "<Text style={styles.plusIcon}>+</Text>" abaixo para criar uma lista <Text style={{color: '#FA4A0C', fontWeight: '900'}}>;)</Text></Text>
+        </View>
+
+        :
+
+        <Lists name='Lista 1' />
+
+      }
 
     </View>
   );
@@ -36,9 +44,15 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 27
   },
-  lists: {
-    flex: 1,
+  noListsView: {
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    marginTop: '80%'
+  },
+  plusIcon: {
+    color: '#FA4A0C',
+    fontSize: 20,
+    fontWeight: 'bold'
   }
 });
