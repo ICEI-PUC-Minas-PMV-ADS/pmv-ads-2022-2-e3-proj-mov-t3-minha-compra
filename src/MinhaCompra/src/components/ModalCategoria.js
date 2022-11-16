e82983e7bb153ca2b12e0d4bcaa2430499dc2fc5
Jhonata Jackson <jhonata.jacksondev@gmail.com>
2022-11-16 19:59:55 -0300

    finaliza implementacao da tela de adicao de produto

--- src/MinhaCompra/src/components/ModalCategoria.js
+++ src/MinhaCompra/src/components/ModalCategoria.js
@@ -1,13 +1,94 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
-export default function ModalCategoria() {
+import SearchBar from "../components/SearchBar";
+import { defaultData } from "../assets/data/default_data";
+
+export default function ModalCategoria({ navigation }) {
+  const [data, setData] = useState(defaultData);
+  const [filteredList, setFilteredList] = useState();
+
+  useEffect(() => {}, []);
+
+  const searchFilterFunction = async (text) => {
+    if (text) {
+      const newList = data.filter((item) => {
+        const listItem = item.tipo;
+        const inputText = text.toLowerCase();
+
+        return listItem.indexOf(inputText) > -1;
+      });
+      setData(newList);
+    }
+  };
+
+  const handleOnChangeText = (event) => {
+    if (event) {
+      searchFilterFunction(event);
+    } else {
+      setData(defaultData);
+    }
+  };
+
+  const Item = ({ title }) => (
+    <TouchableOpacity style={styles.item} onPress={() => select(title)}>
+      <Text>{capitalize(title)}</Text>
+    </TouchableOpacity>
+  );
+
+  const renderItem = ({ item }) => (
+    <View style={{ borderBottomColor: "#00000", borderBottomWidth: 1 }}>
+      <Item title={item.tipo} />
+    </View>
+  );
+
+  const capitalize = (str) => {
+    if (typeof str !== "string") {
+      return "";
+    }
+    return str.charAt(0).toUpperCase() + str.substr(1);
+  };
+
+  const select = (value) => {
+    navigation.navigate("Produto", { categoria: capitalize(value) });
+  };
+
   return (
     <View style={styles.container}>
-      <View>
-        <Text>ModalCategoria</Text>
-      </View>
+      <Text
+        style={{
+          marginTop: 60,
+          marginBottom: 30,
+          textAlign: "center",
+          fontSize: 25,
+        }}
+      >
+        Categoria
+      </Text>
+
+      <SearchBar
+        onChangeText={(event) => {
+          handleOnChangeText(event);
+        }}
+      />
+
+      {/* {data.map((it) => {
+        return <Text> {it.tipo} </Text>;
+      })} */}
+
+      <FlatList
+        style={{ width: "100%" }}
+        data={data}
+        renderItem={renderItem}
+        // keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
@@ -16,7 +97,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    backgroundColor: "#F5F5F8",
+    marginHorizontal: 20,
+  },
+  titleText: {
+    fontSize: 15,
+    marginTop: 20,
+    marginBottom: 27,
+  },
+  noListsView: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 20,
+    marginTop: "80%",
+  },
+  plusIcon: {
+    color: "#FA4A0C",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  item: {
+    width: "100%",
+    height: 35,
+    alignItems: "center",
     justifyContent: "center",
   },
 });
--- src/MinhaCompra/src/components/ModalProduto.js
+++ src/MinhaCompra/src/components/ModalProduto.js
@@ -1,11 +1,95 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
-export default function ModalProduto() {
+import SearchBar from "../components/SearchBar";
+import { defaultData } from "../assets/data/default_data";
+
+export default function ModalProduto({ route, navigation }) {
+  const catName = route?.params?.categoria;
+
+  const [data, setData] = useState([]);
+  const [filteredList, setFilteredList] = useState();
+
+  useEffect(() => {
+    const products = defaultData.filter((product) => product.tipo === catName);
+    setData(products[0].itens);
+    console.log("produtos: ", products[0].itens);
+  }, []);
+
+  const searchFilterFunction = async (text) => {
+    if (text) {
+      const newList = data.filter((item) => {
+        const listItem = item.tipo;
+        const inputText = text.toLowerCase();
+
+        return listItem.indexOf(inputText) > -1;
+      });
+      setData(newList);
+    }
+  };
+
+  const handleOnChangeText = (event) => {
+    if (event) {
+      searchFilterFunction(event);
+    } else {
+      setData(defaultData);
+    }
+  };
+
+  const Item = ({ title }) => (
+    <TouchableOpacity style={styles.item} onPress={() => select(title)}>
+      <Text>{capitalize(title)}</Text>
+    </TouchableOpacity>
+  );
+
+  const renderItem = ({ item }) => (
+    <View style={{ borderBottomColor: "#00000", borderBottomWidth: 1 }}>
+      {console.log("renderItem:", item)}
+      <Item title={item} />
+    </View>
+  );
+
+  const capitalize = (str) => {
+    if (typeof str !== "string") {
+      return "";
+    }
+    return str.charAt(0).toUpperCase() + str.substr(1);
+  };
+
+  const select = (value) => {
+    navigation.navigate("Produto", {
+      produto: capitalize(value),
+      categoria: capitalize(catName),
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>ModaProduto</Text>
+      <Text
+        style={{
+          marginTop: 60,
+          marginBottom: 30,
+          textAlign: "center",
+          fontSize: 25,
+        }}
+      >
+        Tipo
+      </Text>
+
+      <SearchBar
+        onChangeText={(event) => {
+          handleOnChangeText(event);
+        }}
+      />
+
+      <FlatList style={{ width: "100%" }} data={data} renderItem={renderItem} />
     </View>
   );
 }
@@ -14,7 +98,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    backgroundColor: "#F5F5F8",
+    marginHorizontal: 20,
+  },
+  titleText: {
+    fontSize: 15,
+    marginTop: 20,
+    marginBottom: 27,
+  },
+  noListsView: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 20,
+    marginTop: "80%",
+  },
+  plusIcon: {
+    color: "#FA4A0C",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  item: {
+    width: "100%",
+    height: 35,
+    alignItems: "center",
     justifyContent: "center",
   },
 });
